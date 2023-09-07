44e3e70779814ffea3295060c239d4f799bf7fb5
kacperpowalka <kpowalka@gravityglobal.com>
2023-09-07 12:13:25 +0200

    Update despendencies for npm packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,61 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
-const path = require("path");
-const TerserJSPlugin = require("terser-webpack-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const glob = require('glob');
+
+const blocksScssFiles = glob.sync('./includes/blocks/*/assets/styles.scss');
+
+console.log(...blocksScssFiles);
 
 const config = {
   //Your path to dist accessed from a browser
-  distPath: "/wp-content/themes/recruitment-theme/dist/",
+  distPath: '/wp-content/themes/recruitment-theme/dist/',
   //Your local development url for browsersync
-  localUrl: "http://localhost:8080",
-  //Open new window each time 'npm run serve' command is executed 
+  localUrl: 'http://localhost:8080',
+  //Open new window each time 'npm run serve' command is executed
   openWindow: false,
-}
+};
 
 module.exports = {
   optimization: {
     minimizer: [
-      new TerserJSPlugin({
-        sourceMap: true,
+      new TerserPlugin({
+        terserOptions: {
+          sourceMap: true,
+        },
       }),
-      new OptimizeCSSAssetsPlugin({
-        cssProcessorOptions: {
-          map: {
-            inline: false,
-          },
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: ['default', { discardComments: { removeAll: true } }],
         },
       }),
     ],
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "style.css",
+      filename: 'style.css',
     }),
     new BrowserSyncPlugin({
       proxy: config.localUrl,
       files: ['**/*.php'],
       reloadDelay: 0,
       open: config.openWindow,
-    })
+    }),
   ],
   entry: {
-    assets: ["@babel/polyfill", "./src/js/app.js", "./src/scss/app.scss"],
+    assets: [
+      '@babel/polyfill',
+      './src/js/app.js',
+      './src/scss/app.scss',
+      ...blocksScssFiles.map((file) => `./${file}`),
+    ],
   },
   output: {
-    path: path.resolve(__dirname, "./dist/"),    
-    filename: "script.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'script.js',
     publicPath: config.distPath,
   },
   module: {
@@ -53,32 +63,32 @@ module.exports = {
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
           presets: [
             [
-              "@babel/preset-env",
+              '@babel/preset-env',
               {
-                useBuiltIns: "usage",
-                corejs: {version: 3, propsals: true},
+                useBuiltIns: 'usage',
+                corejs: { version: 3, propsals: true },
               },
             ],
           ],
-          cwd: "./",
+          cwd: './',
         },
       },
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
         ],
       },
       {
         test: /\.(jpg|svg|woff|woff2|ttf|eot|otf|png|jpeg)(\?.*$|$)/,
-        loader: "file-loader",
+        loader: 'file-loader',
       },
     ],
   },
